9218f4cde328e2f94f380a12eff1ea9f8cc45544
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-04-19 20:33:21 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int operation = sc.nextInt();
+
+        switch (operation) {
+            case 1:
+                System.out.println(a + b);
+                break;
+
+            case 2:
+                System.out.println(a - b);
+                break;
+
+            case 3:
+                System.out.println(a * b);
+                break;
+
+            case 4:
+                if (b == 0) {
+                    System.out.println("Invalid Operation");
+                } else {
+                    System.out.println(a / b);
+                }
+                break;
+
+            case 5:
+                if (b == 0) {
+                    System.out.println("Invalid Operation");
+                } else {
+                    System.out.println(a % b);
+                }
+                break;
+
+            default:
+                System.out.println("Invalid");
+        }
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World !");
+    }
+}
+
+// final int -> a constant int
