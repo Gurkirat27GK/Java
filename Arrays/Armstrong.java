f80518a03a3ee5122e53757caef51f930c4f183b
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-05-23 11:03:29 +0530

    Update Armstrong.java

--- Arrays/Armstrong.java
+++ Arrays/Armstrong.java
@@ -6,29 +6,29 @@ public class Armstrong {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter starting number");
+        System.out.print("Enter starting number: ");
         int start = sc.nextInt();
 
-        System.out.println("Enter ending number: ");
+        System.out.print("Enter ending number: ");
         int end = sc.nextInt();
 
-        System.out.print("Armstrong numbers between " + start + " and " + end + " are: ");
+        System.out.println("Armstrong numbers between " + start + " and " + end + " are: ");
 
         for (int num = start; num <= end; num++) {
             if (isArmstrong(num)) {
-                System.out.print(num + " ");
+                System.out.println(num);
             }
         }
     }
 
     public static boolean isArmstrong(int num) {
         int originalnum = num;
-        int numDigits = String.valueOf(num).length();
+        int numDigits = String.valueOf(num).length();   //It is used to convert non-string values, such as integers, floats, booleans, objects, into strings
         int sum = 0;
 
         while (num != 0) {
             int digit = num % 10;
-            sum = sum + (int) Math.pow(digit, numDigits);
+            sum = sum + (int) Math.pow(digit, numDigits);   // Math.pow answer double me return krta h, (int) lgane se thik ho jata h
             num = num / 10;
         }
         return sum == originalnum;
