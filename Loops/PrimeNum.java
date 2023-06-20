5b5f0bf206ec5b7fc1b98f002a567e496a0ead4e
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-06-20 22:07:42 +0530

    Update PrimeNum.java

--- Loops/PrimeNum.java
+++ Loops/PrimeNum.java
@@ -4,23 +4,19 @@ public class PrimeNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int i = 2;
+        int d = 2;
 
         if (n == 0 || n == 1) {
             System.out.println("Not Prime");
         }
 
-        else if (n == i) {
-            System.out.println("Prime");
+        while (d <= n - 1) {
+            if (n % d == 0) {
+                System.out.println("Not Prime");
+                return;
+            }
+            d = d + 1;
         }
-
-        else if (n % i == 0) {
-            System.out.println("Not Prime");
-            i++;
-        }
-
-        else {
-            System.out.println("Prime");
-        }
+        System.out.println("Prime");
     }
 }
