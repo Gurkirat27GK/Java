8dc60fbc770cbce6a69d295ba7a7279ca3872df7
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-06-22 01:22:22 +0530

    Add files via upload

---
+++ Patterns/Pattern5.java
@@ -0,0 +1,26 @@
+/*
+ 4444
+ 4444
+ 4444
+ 4444
+ */
+
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+
+        while (i <= n) {
+            int j = 1;
+            while (j <= n) {
+                System.out.print(n);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
