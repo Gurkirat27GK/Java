754f93e152d2c6ae45508c810a0a8f70cfae3906
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-06-24 00:35:29 +0530

    Add files via upload

---
+++ Patterns/Pattern12.java
@@ -0,0 +1,26 @@
+/*
+ ABCD
+ ABCD
+ ABCD
+ ABCD
+ */
+
+import java.util.Scanner;
+
+public class Pattern12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+
+        while (i <= n) {
+            int j = 1;
+            while (j <= n) {
+                System.out.print((char) ('A' + j - 1));
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
