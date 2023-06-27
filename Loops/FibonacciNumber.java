f4e85d5c78c0e2ce7ea6c3809529295f8d175666
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-06-27 17:36:35 +0530

    Add files via upload

---
+++ Loops/FibonacciNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int c;
+
+        for (int i = 0; i < n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        System.out.println(a);
+    }
+}
