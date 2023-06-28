1d21596deafc30e254bb031aa075359e16740783
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-06-29 00:54:44 +0530

    Add files via upload

---
+++ Loops/SquareRoot.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int output = 0;
+
+        while (output * output <= n) {
+            output++;
+        }
+        output--;
+        System.out.println(output);
+    }
+}
