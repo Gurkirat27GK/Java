d060930664db3213d1f4520b05c90323abf73ece
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-07-07 19:48:19 +0530

    Add files via upload

---
+++ Patterns/Pattern26.java
@@ -0,0 +1,33 @@
+/*
+ ****
+  ****
+   ****
+    ****
+ */
+
+import java.util.Scanner;
+
+public class Pattern26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+
+        while (i <= n) {
+            int spaces = 1;
+            while (spaces <= i - 1) {
+                System.out.print(" ");
+                spaces++;
+            }
+
+            int j = 1;
+            while (j <= n) {
+                System.out.print("*");
+                j++;
+            }
+
+            System.out.println();
+            i++;
+        }
+    }
+}
