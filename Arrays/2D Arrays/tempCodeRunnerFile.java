c3fab2b2d7d2967a80ffe1b31142fa289a7ab983
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-08-11 23:29:12 +0530

    Delete tempCodeRunnerFile.java

--- Arrays/2D Arrays/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-for (int i = 0; i < rows; i++) {
-        // for (int j = 0; j < cols; j++) {
-        // System.out.print(drr[i][j] + " ");
-        // }
-        // System.out.println();
-        // }
