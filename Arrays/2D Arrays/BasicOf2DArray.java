d7e932789e36d4324b19661ed87a8601a8a996f3
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-08-11 23:28:49 +0530

    Add files via upload

---
+++ Arrays/2D Arrays/BasicOf2DArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BasicOf2DArray {
+    public static void main(String args[]) {
+        int arr[][] = new int[3][4];
+        System.out.println(arr[1][2]);
+
+        arr[2][1] = 12;
+        System.out.println(arr[2][1]);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of rows");
+        int rows = sc.nextInt();
+
+        System.out.println("Enter no. of columns");
+        int cols = sc.nextInt();
+
+        int drr[][] = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.println("Enter element at " + i + " row " + j + " column ");
+                drr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(drr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Arrays/2D Arrays/tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+for (int i = 0; i < rows; i++) {
+        // for (int j = 0; j < cols; j++) {
+        // System.out.print(drr[i][j] + " ");
+        // }
+        // System.out.println();
+        // }
