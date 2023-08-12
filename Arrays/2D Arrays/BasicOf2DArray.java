80143f9a71a9ffbd1cf1b687b409c98a2c1737b5
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-08-12 19:36:04 +0530

    Update BasicOf2DArray.java

--- Arrays/2D Arrays/BasicOf2DArray.java
+++ Arrays/2D Arrays/BasicOf2DArray.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class BasicOf2DArray {
-    public static void main(String args[]) {
-        int arr[][] = new int[3][4];
-        System.out.println(arr[1][2]);
 
-        System.out.println(arr.length); // No. of rows
-        System.out.println(arr[0].length); // No. of columns
+    public static void print2dArray(int[][] drr) {
+        int rows = drr.length;
+        int cols = drr[0].length;
 
-        arr[2][1] = 12;
-        System.out.println(arr[2][1]);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(drr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
+    public static int[][] takeInput() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter no. of rows");
         int rows = sc.nextInt();
@@ -26,12 +30,20 @@ public class BasicOf2DArray {
                 drr[i][j] = sc.nextInt();
             }
         }
+        return drr;
+    }
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(drr[i][j] + " ");
-            }
-            System.out.println();
-        }
+    public static void main(String args[]) {
+        int arr[][] = new int[3][4];
+        System.out.println(arr[1][2]);
+
+        System.out.println(arr.length); // No. of rows
+        System.out.println(arr[0].length); // No. of columns
+
+        arr[2][1] = 12;
+        System.out.println(arr[2][1]);
+
+        int input[][] = takeInput();
+        print2dArray(input);
     }
 }
