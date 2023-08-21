507fa2ae6eafe156061f49efff0dc24110cd1b7b
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-08-21 15:40:14 +0530

    Add files via upload

---
+++ Recursion/SumOfNaturalNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+
+    public static int sum(int n) {
+        // Base Case
+        if (n == 1) {
+            return 1;
+        }
+
+        return n + sum(n - 1);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int ans = sum(n);
+        System.out.println(ans);
+    }
+}
