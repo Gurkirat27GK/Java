376269b6be2186ee66ffc1b13af50751448cd263
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-08-24 21:20:13 +0530

    Add files via upload

---
+++ Recursion/FibonacciNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FibonacciNumber {
+
+    public static int fib(int n) {
+        // Base Case
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int ans = fib(n);
+        System.out.println(ans);
+    }
+}
