d814495a85a930840d15fe1574e15b24a083ef5d
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-09-12 17:12:47 +0530

    Add files via upload

---
+++ Recursion/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+    public static String removeDuplicates(String str) {
+
+        // Base Case
+        if (str.length() <= 1) {
+            return str;
+        }
+
+        if (str.charAt(0) == str.charAt(1)) {
+            return removeDuplicates(str.substring(1));
+        } else {
+            String smallAns = removeDuplicates(str.substring(1));
+            return str.charAt(0) + smallAns;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println(removeDuplicates(str));
+    }
+}
