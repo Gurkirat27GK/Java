6ada76fb6b4f4b052bda43dd0d601ad0c282a116
Gurkirat Singh <93971485+Gurkirat27GK@users.noreply.github.com>
2023-10-13 18:55:01 +0530

    Add files via upload

---
+++ Arrays/Sorting/SelectionSort.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            // Inserting element at ith position
+            int min = Integer.MAX_VALUE;
+            int minIndex = -1;
+
+            for (int j = i; j < n; j++) {
+                // Finding the minimum element among j positions
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            // Swap element at minimum element with ith element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        selectionSort(arr);
+        printArray(arr);
+    }
+}
